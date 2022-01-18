319ebd8653bae06b05ef3a17d6796c5633ff2cf3
Vicente Sepulveda <vicente.sepulveda.g@usach.cl>
2022-01-18 01:07:09 -0300

    Insertar

--- src/componentes/ProductoComponente.js
+++ src/componentes/ProductoComponente.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import React,{useState} from 'react';
 import ProductoServicio from '../servicios/ProductoServicio';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 class ProductoComponente extends React.Component{
 
     constructor(props){
         super(props)
         this.state = {
-            productos:[]
+            productos:[],
+            modalInsertar: false,
+            form:{
+                    codigo:'',
+                    nombre:'',
+                    fechaVencimiento: new Date(),
+                    categoria:'',
+                    precio:'',
+            }
         }
     }
 
@@ -16,40 +28,99 @@ class ProductoComponente extends React.Component{
             this.setState({productos:response.data})
         });
     }
-
-
+    deleteProducto = async (producto) => {
+        await  axios.delete('http://localhost:8080/api/producto/'+ producto)
+        this.componentDidMount();
+    }
+    modalInsertar=()=>{
+        this.setState({modalInsertar:!this.state.modalInsertar});
+    }
+    handleChange=async e=>{
+        e.persist();
+        await this.setState({
+          form:{
+            ...this.state.form,
+            [e.target.name]: e.target.value
+          }
+        });
+        console.log(this.state.form);
+    }
+    dateChange=fecha=>{
+        this.setState({fecha:fecha});
+    }
+    peticionPost=async()=>{
+        await axios.post('http://localhost:8080/api/producto/',this.state.form).then(response=>{
+            this.modalInsertar();
+            this.componentDidMount();
+        }).catch(error=>{
+            console.log(error.message);
+        })
+    }
     render(){
+        const {form} = this.state;
         return(
             <div>
                 <h1 className='text-center' >Lista De Productos</h1>
-                <button style={{position: 'fixed', top: 20, right: 0,borderRadius:50}}>Añadir producto</button>
+                <button style={{position: 'fixed', top: 20, right: 0,borderRadius:50}} onClick={this.modalInsertar}>Añadir producto</button>
                 <table className='table table-striped'>
                     <thead>
                         <tr>
-                            <td>Id producto</td>
                             <td>Codigo producto</td>
                             <td>Nombre producto</td>
                             <td>Fecha de vencimiento producto</td>
                             <td>Categoria producto</td>
                             <td>Precio producto</td>
+                            <td>Eliminar Producto</td>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             this.state.productos.map(
                                 producto =>
-                                <tr key={producto.id}>
+                                <tr >
                                     <td>{producto.codigo}</td>
                                     <td>{producto.nombre}</td>
                                     <td>{producto.fechaVencimiento}</td>
                                     <td>{producto.categoria}</td>
                                     <td>{producto.precio}</td>
+                                    <button style={{borderRadius:50}}onClick={() => this.deleteProducto(producto)}>Delete</button>
                                 </tr>
                             )
                         }
                     </tbody>
                 </table>
                 
+
+                <Modal isOpen={this.state.modalInsertar}>
+                    <ModalHeader style = {{display: 'block'}}>
+                        <span style={{float:'right'}}>x</span>
+                    </ModalHeader>
+                    <ModalBody>
+                        <div className='form-group'>
+                            <label htmlFor='codigo'>Codigo</label>
+                            <input className='form-control' type="text" name="codigo" id="codigo" onChange={this.handleChange} value={form.codigo}/>
+                            <br/>
+                            <label htmlFor='nombre'>Nombre</label>
+                            <input className='form-control' type="text" name="nombre" id="nombre" onChange={this.handleChange} value={form.nombre}/>
+                            <br/>
+                            <label htmlFor='nombre'>Fecha</label>
+                            <DatePicker value={form.fechaVencimiento}/>
+                            <br/>
+                            <label htmlFor='categoria'>Categoria</label>
+                            <input className='form-control' type="text" name="categoria" id="categoria" onChange={this.handleChange}value={form.categoria}/>
+                            <br/>
+                            <label htmlFor='precio'>Precio</label>
+                            <input className='form-control' type="text" name="precio" id="precio" onChange={this.handleChange}value={form.precio}/>
+                            <br/>
+                        </div>
+                    </ModalBody>
+                    <ModalFooter>
+                                <button className="btn btn-success" onClick={()=>this.peticionPost()}>
+                                Insertar
+                            </button>
+                                <button className="btn btn-danger" onClick={()=>this.modalInsertar()}>Cancelar</button>
+                </ModalFooter>
+                </Modal>
             </div>
             )
     }
